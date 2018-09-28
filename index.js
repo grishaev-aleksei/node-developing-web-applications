270163e0f48573156c925e8c1f8342ff9eaf58a4
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2018-09-28 23:02:00 +0300

    lesson 4 done

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
-var helloWorld = 'hello world';
-var info = 'my name is aleksei and this is my first app'
+const helloWorld = 'hello world';
+const info = 'my name is Aleksei and this is my first app';
+
+http.createServer(function (request, responce) {
+    responce.writeHead(200, {contentType:'text:html'});
+    responce.write('<h1>'+ helloWorld +'</h1>');
+    responce.write('<p>' +info + '</p>');
+    responce.end();
+
+}).listen(3000);
+
+fs.writeFile('data.txt', helloWorld + "\n" + info, function (err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log('Data successfully created');
+});
