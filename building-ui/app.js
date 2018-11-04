807771d8f2b720d58b21b8c1b4833b8e5a8918b2
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2018-11-04 11:18:03 +0300

    building UI (43)

---
+++ building-ui/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home.hbs');
+});
+
+app.get('/:username', (req, res) => {
+    const username = req.params.username;
+    res.render('username.hbs', {username});
+});
+
+app.get('/:username/:city/:state', (req, res) => {
+    const username = req.params.username;
+    const city = req.params.city;
+    const state = req.params.state;
+    res.render('info.hbs', {
+        username,
+        city,
+        state,
+        date: new Date()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('listening on port = 3000 ....')
+});
