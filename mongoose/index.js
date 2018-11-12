711435e25fea762bce50dbfdf5dee5fa1b493503
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2018-11-12 09:32:52 +0300

    mongoose update item (52)

--- mongoose/index.js
+++ mongoose/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const mongooseSave = require('./mongooseSave');
 const mongooseFind = require('./mongooseFind');
 const mongooseDelete = require('./mongooseDelete');
+const mongooseUpdate = require('./mongooseUpdate');
 const ShoppingList = require('./models/shoppingList');
 
 mongoose.connect('mongodb://localhost:27017/myproject', {useNewUrlParser: true}).then(
@@ -17,10 +18,26 @@ const listItem = new ShoppingList({
     quantity: 7
 });
 
+const obj = {
+    item: 'oranges',
+    quantity: 22
+};
+
+const unsetObj = {
+    quantity: ""
+};
+
+const incObj = {
+    quantity: +2
+};
+
 // mongooseSave.saveItem(listItem);
 // mongooseFind.findAll();
 // mongooseFind.findItem(listItem.item);
 // mongooseFind.findById('5be69c4139be7d036a9d5057');
 // mongooseDelete.deleteAll();
 // mongooseDelete.deleteSingle('cucumbers');
-mongooseDelete.showDeletedItem('5be7c1fd7f8d3c02fe47731b');
+// mongooseDelete.showDeletedItem('5be7c1fd7f8d3c02fe47731b');
+// mongooseUpdate.updateSet('apples', 7, obj);
+// mongooseUpdate.updateUnset('apples', unsetObj);
+// mongooseUpdate.updateInc('cucumbers', incObj);
---
+++ mongoose/mongooseUpdate.js
@@ -0,0 +1,43 @@
+const ShoppingList = require('./models/shoppingList');
+
+const updateSet = (item, quantity, obj) => {
+    ShoppingList.findOneAndUpdate({
+        item, quantity
+    }, {
+        $set: obj
+    }).then((result) => {
+        console.log(result)
+    }, (err) => {
+        console.log(err)
+    })
+};
+
+const updateUnset = (item, unsetObj) => {
+    ShoppingList.findOneAndUpdate({
+        item
+    }, {
+        $unset: unsetObj
+    }).then((result) => {
+        console.log(result)
+    }, (err) => {
+        console.log(err)
+    })
+};
+
+const updateInc = (item, incObj) => {
+    ShoppingList.findOneAndUpdate({
+        item
+    }, {
+        $inc: incObj
+    }).then((result) => {
+        console.log(result)
+    }, (err) => {
+        console.log(err)
+    })
+};
+
+module.exports = {
+    updateSet,
+    updateUnset,
+    updateInc
+};
