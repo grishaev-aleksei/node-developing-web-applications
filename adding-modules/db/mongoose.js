3ee1de7096905c5e8ba6ca339a574f3a66d13b4c
grishaev-aleksei <aleksey.grishaev.msk@gmail.com>
2018-11-14 12:11:22 +0300

    adding modules (56)

---
+++ adding-modules/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/myproject';
+
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+    console.log('connected to myproject collection')
+}, (err) => {
+    console.log(err)
+});
